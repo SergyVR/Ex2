66c9ace1f12c4eb78636b00f48ed8b4e65233df9
SergyRV <rsv1177@gmail.com>
2022-10-19 22:32:34 +0300

    first commit

---
+++ Ex11/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+//показывает вторую цифру этого числа.
+// 456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Введите трёхзначное число");
+string Number = Console.ReadLine();
+int Num = int.Parse(Number); 
+
+var digits = Number.ToString().Select(t=>int.Parse(t.ToString())).ToArray();
+Console.WriteLine(digits[1]);
---
+++ Ex3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите число");
+string Number = Console.ReadLine();
+int Num = int.Parse(Number); 
+
+var digits = Number.ToString().Select(t=>int.Parse(t.ToString())).ToArray();
+int n = digits.Length;
+
+if(n<3)
+{
+Console.WriteLine("третьей цифры нет"); 
+}
+else
+{Console.WriteLine(digits[2]);}
